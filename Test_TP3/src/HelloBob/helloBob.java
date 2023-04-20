631f710f38f092f17f9ed7689d7ea1ed834ced38
YoannSME <yo.smets@orange.fr>
2023-04-20 17:09:06 +0200

    ex1

---
+++ Test_TP3/src/HelloBob/helloBob.java
@@ -0,0 +1,11 @@
+package HelloBob;
+
+public class helloBob {
+	
+	public static Object hello(String name) {
+		if(name.equals("bob"))
+			return "Hello, Bob";
+		return "";
+	}
+
+}
---
+++ Test_TP3/test/HelloBob/helloBobTest.java
@@ -0,0 +1,19 @@
+package HelloBob;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class helloBobTest {
+
+	@Test
+	void testHello1() {
+		assertEquals(helloBob.hello("bob"),"Hello, Bob");
+	}
+	
+	@Test
+	void testHello2() {
+		
+	}
+
+}
