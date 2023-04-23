9551742690af84e78cf95d5d45754b9d051fb66b
YoannSME <yo.smets@orange.fr>
2023-04-23 23:12:35 +0200

    CleanCode

--- Test_TP3/src/HelloBob/helloBob.java
+++ Test_TP3/src/HelloBob/helloBob.java
@@ -3,7 +3,46 @@ package HelloBob;
 public class helloBob {
 	
 	public static Object hello(String name) {
+		name = name.replaceAll("\\s","");
+		if(name.trim().equals(""))
+			return "Hello, my friend";
+		if(name.toUpperCase().equals(name))
+			return "HELLO, "+name+" !";
+		if(name.contains(",")) {
+			return verifVirg(name);
+		}
+		String nomMaj = name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+		return "Hello, "+nomMaj;
+	}
+	
+	
+	private static String verifVirg(String name) {
 		String maj = ". AND HELLO,";
+		String retour ="Hello, ";
+		String[] split = name.split(",");
+		for(String word : split) {
+			if(word.toUpperCase().equals(word)) {
+				maj+=word;
+			}
+			else {
+			word = word.substring(0,1).toUpperCase()+word.substring(1).toLowerCase();
+			retour+= word+", ";
+			}
+	}
+	retour = retour.substring(0,retour.length()-2);
+		if(maj.equals(". AND HELLO,")) 
+	return retour;
+		else
+			maj+= " !";
+			retour+= maj;
+		return retour;
+		
+		
+	}
+	
+	public static Object helloAnd(String name) {
+		String maj = ". AND HELLO,";
+		String[] nomMajus;
 		name = name.replaceAll("\\s","");
 		if(name.trim().equals(""))
 			return "Hello, my friend";
