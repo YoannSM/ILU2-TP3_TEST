51c2d497473cb93526a23f4b93f3e91b2bcb7fe0
YoannSME <yo.smets@orange.fr>
2023-04-23 22:36:14 +0200

    Fin EX5

--- Test_TP3/src/HelloBob/helloBob.java
+++ Test_TP3/src/HelloBob/helloBob.java
@@ -3,13 +3,22 @@ package HelloBob;
 public class helloBob {
 	
 	public static Object hello(String name) {
-		String retour;
+		name = name.replaceAll("\\s","");
 		if(name.trim().equals(""))
 			return "Hello, my friend";
 		if(name.toUpperCase().equals(name))
 			return "HELLO, "+name+" !";
-		if(name.equals("amy,bob"))
-			return "Hello, Amy,Bob";
+		if(name.contains(",")) {
+			String retour ="Hello, ";
+			String[] split = name.split(",");
+			for(String word : split) {
+				word = word.substring(0,1).toUpperCase()+word.substring(1).toLowerCase();
+				retour+= word+", ";
+		}
+		retour = retour.substring(0,retour.length()-2);
+		return retour;
+		}
+		
 		String nomMaj = name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
 		return "Hello, "+nomMaj;
 	}
--- Test_TP3/test/HelloBob/helloBobTest.java
+++ Test_TP3/test/HelloBob/helloBobTest.java
@@ -30,13 +30,34 @@ class helloBobTest {
 	}
 	
 	@Test
+	void gestionNullandSpace4() {
+		assertEquals(helloBob.hello("    bob     "),"Hello, Bob");
+	}
+	
+	@Test
 	void gestionCri() {
 		assertEquals(helloBob.hello("JERRY"),"HELLO, JERRY !");
 	}
 	
 	@Test
 	void gestionDeuxNoms() {
-		assertEquals(helloBob.hello("amy,bob"),"Hello, Amy,Bob");
+		assertEquals(helloBob.hello("amy,bob"),"Hello, Amy, Bob");
+	}
+	
+	@Test
+	void gestionDeuxNomsSpace() {
+		assertEquals(helloBob.hello("  amy   ,  bob"),"Hello, Amy, Bob");
+	}
+	
+	@Test
+	void gestionPlusieursNoms() {
+		assertEquals(helloBob.hello("amy,bob,jerry"),"Hello, Amy, Bob, Jerry");
+	}
+	void gestionPlusieursNomsSpace() {
+		assertEquals(helloBob.hello("a my,bob,jer ry"),"Hello, Amy, Bob, Jerry");
+	}
+	void gestionPlusieursNomsMajAndSpace() {
+		assertEquals(helloBob.hello("Amy,bob   ,jer ry"),"Hello, Amy, Bob, Jerry");
 	}
 	
 
